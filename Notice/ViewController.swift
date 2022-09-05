b3f66ca51b82ac5affdaffa6f76c805a21af781d
RaeHoon Baek <bmp117@naver.com>
2022-09-05 20:04:53 +0900

    Notice 예제 - A_B Test로 팝업 제어하기

--- Notice/ViewController.swift
+++ Notice/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseRemoteConfig
+import FirebaseAnalytics
 
 class ViewController: UIViewController {
 
@@ -59,6 +60,8 @@ extension ViewController {
             
                 noticeVC.noticeContents = (title: title, detail: detail, date: date)
                 self.present(noticeVC, animated: true, completion: nil)
+            } else {
+                self.showEventAlert()
             }
         }
     }
@@ -66,7 +69,35 @@ extension ViewController {
     func isNoticeHidden(_ remoteConfig: RemoteConfig) -> Bool {
         return remoteConfig["isHidden"].boolValue
     }
-    
-    
 }
 
+//A/B Testing
+extension ViewController {
+    func showEventAlert() {
+        guard let remoteConfig = remoteConfig else { return }
+        
+        remoteConfig.fetch { [weak self] status, _ in
+            if status == .success {
+                remoteConfig.activate(completion: nil)
+            } else {
+                print("Config not fetched")
+            }
+            
+            let message = remoteConfig["message"].stringValue ?? ""
+            
+            let confirmAction = UIAlertAction(title: "확인하기", style: .default) { _ in
+                //Google Analytics
+                Analytics.logEvent("promotion_alert", parameters: nil)
+                
+            }
+            
+            let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+            let alertController = UIAlertController(title: "깜짝이벤트", message: message, preferredStyle: .alert)
+            
+            alertController.addAction(confirmAction)
+            alertController.addAction(cancelAction)
+            
+            self?.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
